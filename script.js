dcc093309240ba246b88d599f3934f53d1a11c90
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2022-10-16 20:15:07 -0500

    added confetti when winning

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function matchCards(img1, img2) {
         matchedPairs++; // if the card images match, we can increment the global `matchedPairs` variable by 1 match
         if (matchedPairs == 8) { // if your number of matches is 8, you've made all the matches! Game Won!
             console.log('YOU WIN!');
+            confetti(); // execute the confetti function when the user wins the game
             return; // for now, lets call this game over, end this function and do nothing else.
         }
         // everything below will execute if the game has not yet been won...
@@ -63,6 +64,132 @@ function shuffleCards() {
         let imgTag = card.querySelector(".back-view img"); // find the back-view image tag by querying all the childNodes of the current card element for the '.back-view img' CSS selector
         imgTag.src = `images/img-${arr[i]}.png`; // set the value of the src attribute on the current imgTag to a numbered filename based on our randomized array
         card.addEventListener("click", flipCard); // add the click event listener to the current card to execute a function `flipCard` when clicked
+    });
+}
+
+function confetti() {
+    //-----------Var Inits--------------
+    canvas = document.getElementById("canvas");
+    canvas.classList.add("active"); // set active to show the confetti
+    ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    cx = ctx.canvas.width / 2;
+    cy = ctx.canvas.height / 2;
+
+    let confetti = [];
+    const confettiCount = 300;
+    const gravity = 0.5;
+    const terminalVelocity = 5;
+    const drag = 0.075;
+    const colors = [
+        { front: 'red', back: 'darkred' },
+        { front: 'green', back: 'darkgreen' },
+        { front: 'blue', back: 'darkblue' },
+        { front: 'yellow', back: 'darkyellow' },
+        { front: 'orange', back: 'darkorange' },
+        { front: 'pink', back: 'darkpink' },
+        { front: 'purple', back: 'darkpurple' },
+        { front: 'turquoise', back: 'darkturquoise' }];
+
+
+    //-----------Functions--------------
+    resizeCanvas = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        cx = ctx.canvas.width / 2;
+        cy = ctx.canvas.height / 2;
+    };
+
+    randomRange = (min, max) => Math.random() * (max - min) + min;
+
+    initConfetti = () => {
+        for (let i = 0; i < confettiCount; i++) {
+            confetti.push({
+                color: colors[Math.floor(randomRange(0, colors.length))],
+                dimensions: {
+                    x: randomRange(10, 20),
+                    y: randomRange(10, 30)
+                },
+
+                position: {
+                    x: randomRange(0, canvas.width),
+                    y: canvas.height - 1
+                },
+
+                rotation: randomRange(0, 2 * Math.PI),
+                scale: {
+                    x: 1,
+                    y: 1
+                },
+
+                velocity: {
+                    x: randomRange(-25, 25),
+                    y: randomRange(0, -50)
+                }
+            });
+
+
+        }
+    };
+
+    //---------Render-----------
+    render = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        confetti.forEach((confetto, index) => {
+            let width = confetto.dimensions.x * confetto.scale.x;
+            let height = confetto.dimensions.y * confetto.scale.y;
+
+            // Move canvas to position and rotate
+            ctx.translate(confetto.position.x, confetto.position.y);
+            ctx.rotate(confetto.rotation);
+
+            // Apply forces to velocity
+            confetto.velocity.x -= confetto.velocity.x * drag;
+            confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
+            confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
+
+            // Set position
+            confetto.position.x += confetto.velocity.x;
+            confetto.position.y += confetto.velocity.y;
+
+            // Delete confetti when out of frame
+            if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
+
+            // Loop confetto x position
+            if (confetto.position.x > canvas.width) confetto.position.x = 0;
+            if (confetto.position.x < 0) confetto.position.x = canvas.width;
+
+            // Spin confetto by scaling y
+            confetto.scale.y = Math.cos(confetto.position.y * 0.1);
+            ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
+
+            // Draw confetti
+            ctx.fillRect(-width / 2, -height / 2, width, height);
+
+            // Reset transform matrix
+            ctx.setTransform(1, 0, 0, 1, 0, 0);
+        });
+
+        // Fire off another round of confetti
+        if (confetti.length <= 10) initConfetti();
+
+        window.requestAnimationFrame(render);
+    };
+
+    //---------Execution--------
+    initConfetti();
+    render();
+
+    //----------Resize----------
+    window.addEventListener('resize', function () {
+        resizeCanvas();
+    });
+
+    //------------Click------------
+    window.addEventListener('click', function () {
+        initConfetti();
     });
 }
 
